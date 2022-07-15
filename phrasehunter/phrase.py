954918232e4dbc0985b2af64e1d768af0c646a22
David C Hernandez <58623347+dhernandez-msjc@users.noreply.github.com>
2022-07-15 09:53:23 -0700

    Update phrase.py

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -3,7 +3,13 @@ class Phrase:
     def __init__(self, phrase: str) -> None:
         self.phrase = phrase.lower()
 
-    def display(self, guesses) -> None:
+    def display(self, guesses: [str]) -> None:
+        """
+        Displays which of the letters of the phrase the user
+        has guessed correctly, as well as the shape of the phrase.
+        :param guesses: The user guesses
+        :return: None, prints message
+        """
         for letter in self.phrase:
             if letter in guesses:
                 print(f'{letter}', end='')
@@ -11,10 +17,23 @@ class Phrase:
                 print('_', end='')
         print()
 
-    def check_guess(self, guess) -> bool:
+    def check_guess(self, guess: str) -> bool:
+        """
+        Checks the user guess against the phrase to see if
+        the guessed letter is in the phrase.
+        :param guess: The letter the user is guessing.
+        :return: bool representing True if the letter is in the
+                 phrase, False otherwise.
+        """
         return guess in self.phrase
 
-    def check_complete(self, guesses) -> bool:
+    def check_complete(self, guesses: [str]) -> bool:
+        """
+        Checks if the phrase has been completely guessed.
+        :param guesses: The list of letters the player has guessed.
+        :return: True if all letters are in the guesses list,
+                 False otherwise.
+        """
         for letter in self.phrase:
             if letter not in guesses:
                 return False
